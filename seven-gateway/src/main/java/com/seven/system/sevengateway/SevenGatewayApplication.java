068ade2a604ad78a5b98e2f8d909c106d0131ab0
凶猛的大铁锤 <893371542@qq.com>
2026-07-12 15:18:10 +0800

    Initial commit

---
+++ seven-gateway/src/main/java/com/seven/system/sevengateway/SevenGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.seven.system.sevengateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenGatewayApplication.class, args);
+    }
+
+}
---
+++ seven-gateway/src/test/java/com/seven/system/sevengateway/SevenGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seven.system.sevengateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
