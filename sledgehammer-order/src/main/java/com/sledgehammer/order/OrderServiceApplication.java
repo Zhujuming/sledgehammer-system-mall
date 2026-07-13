820635db106d6a612e6331eff1816d90a52dc2ce
sledgehammer <893371542@qq.com>
2026-07-14 00:23:58 +0800

    feat(order): 添加订单服务模块
    - 创建了订单服务的Spring Boot应用程序入口类
    - 配置了订单服务的application.properties文件
    - 添加了订单服务的测试类OrderServiceApplicationTests
    - 在父pom.xml中注册sledgehammer-order模块
    - 统一项目groupId从com.seven到com.sledgehammer
    - 配置订单服务的基础依赖和构建插件
    - 添加了项目的依赖管理配置

---
+++ sledgehammer-order/src/main/java/com/sledgehammer/order/OrderServiceApplication.java
@@ -0,0 +1,22 @@
+package com.sledgehammer.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot应用程序的主入口类
+ * 使用@SpringBootApplication注解标记这是一个Spring Boot应用
+ */
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    /**
+     * 程序的主方法，应用程序的执行起点
+     * @param args 命令行参数
+     */
+	public static void main(String[] args) {
+        // 使用SpringApplication.run()方法启动Spring Boot应用程序
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
