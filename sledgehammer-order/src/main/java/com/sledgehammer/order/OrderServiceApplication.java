4b439337139dc364b4909200593d8a2ca33f5e6d
sledgehammer <893371542@qq.com>
2026-07-19 16:49:58 +0800

    feat(config): 集成Nacos配置中心和服务发现
    - 在gateway、order、pay三个服务中添加Nacos配置中心和服务发现依赖
    - 配置application.yml文件以连接到Nacos服务器并启用配置导入
    - 为order服务添加@RefreshScope注解以支持配置热更新
    - 在order控制器中添加获取配置信息的测试接口
    - 创建sledgehammer-dev.env环境变量文件存储Nacos凭据
    - 更新项目依赖注释以明确区分注册中心和配置中心功能

--- sledgehammer-order/src/main/java/com/sledgehammer/order/OrderServiceApplication.java
+++ sledgehammer-order/src/main/java/com/sledgehammer/order/OrderServiceApplication.java
@@ -2,11 +2,13 @@ package com.sledgehammer.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * Spring Boot应用程序的主入口类
  * 使用@SpringBootApplication注解标记这是一个Spring Boot应用
  */
+@RefreshScope  // 使配置文件热加载（Nacos动态刷新）
 @SpringBootApplication
 public class OrderServiceApplication {
 
--- sledgehammer-order/src/main/java/com/sledgehammer/order/controller/OrderController.java
+++ sledgehammer-order/src/main/java/com/sledgehammer/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.sledgehammer.order.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
  * 订单控制器类
  * 用于处理与订单相关的请求和业务逻辑
  */
+@RefreshScope
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
+    @Value("${config.info}")
+   private String configInfo;
+
     @GetMapping("/test")
     public String test() {
         return "this is order service";
+    }
+
+    @GetMapping("/test/getConfigInfo")
+    public String getConfigInfo() {
+        return configInfo;
     }
 }
