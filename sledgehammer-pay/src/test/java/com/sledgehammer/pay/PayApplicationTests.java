8184d7c6590145842d8d434b8fae28b1a84580ec
sledgehammer <893371542@qq.com>
2026-07-19 23:49:25 +0800

    refactor(test): 重命名测试类名称
    - 将 SledgehammerPayApplicationTests 重命名为 PayApplicationTests
    - 更新测试类文件名以匹配新的类名
    - 保持测试功能不变，仅优化类命名规范

--- sledgehammer-pay/src/test/java/com/sledgehammer/pay/PayApplicationTests.java
+++ sledgehammer-pay/src/test/java/com/sledgehammer/pay/PayApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SledgehammerPayApplicationTests {
+class PayApplicationTests {
 
     @Test
     void contextLoads() {
